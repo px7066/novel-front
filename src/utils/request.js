672624c11b933469f42bf24013d9cb3e0bc65812
7066450@qq.com <7066450@qq.com>
2020-05-02 19:10:19 +0800

    超时时间改为不超时

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { Toast } from 'mint-ui'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 0 // request timeout
   // withCredentials: true,
   // crossDomain: true
 })
